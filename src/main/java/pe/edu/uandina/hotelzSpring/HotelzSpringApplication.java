98e65780a2ec3fd1ae878c7f1d497b0633a1bdc7
Rodrigo Aguirre <rodri1996991@gmail.com>
2021-11-14 12:50:49 -0800

    first commit 14/11/2021

---
+++ src/main/java/pe/edu/uandina/hotelzSpring/HotelzSpringApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.uandina.hotelzSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelzSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelzSpringApplication.class, args);
+	}
+
+}
